f0faf011b6f6d6c91095c0694d38f7f0dcda45e6
Ekaterina Shigina <shiginaeo@yandex.ru>
2022-08-08 22:56:51 +0300

    add test

---
+++ src/main/java/ru/yandex/practicum/Account.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum;
+
+import io.qameta.allure.Step;
+
+import java.util.regex.Pattern;
+
+public class Account {
+
+    private final String name;
+    private final Pattern pattern = Pattern.compile("(?=(^\\S+\\s\\S+$))(.{3,19})");
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    @Step("Проверка введенного имени на соответсвие паттерну")
+    public boolean checkNameToEmboss() {
+        /*
+             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
+             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
+         */
+
+        return  name != null
+                && pattern.matcher(name).matches();
+
+    }
+
+}
